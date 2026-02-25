e7f929c96f232d6c9e63ce7f932089de44f09364
Kevin Lu <kevinlu3234@gmail.com>
2026-02-25 06:13:01 -0800

    fixing the data set live

--- ai4sar-webpage/lib/firebase-admin.ts
+++ ai4sar-webpage/lib/firebase-admin.ts
@@ -1,26 +1,14 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-let adminDb: ReturnType<typeof getFirestore>;
-
 if (!getApps().length) {
-  const projectId = process.env.FIREBASE_PROJECT_ID;
-  const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-  const privateKey = process.env.FIREBASE_PRIVATE_KEY;
-
-  if (projectId && clientEmail && privateKey) {
-    initializeApp({
-      credential: cert({
-        projectId,
-        clientEmail,
-        privateKey: privateKey.replace(/\\n/g, "\n"),
-      }),
-    });
-
-    adminDb = getFirestore();
-  }
-} else {
-  adminDb = getFirestore();
+  initializeApp({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
 }
 
-export { adminDb };
+export const db = getFirestore();
