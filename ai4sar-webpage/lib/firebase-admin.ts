218c621c96b4e925e0de6ba1c8f377d1b6d99470
Kevin Lu <kevinlu3234@gmail.com>
2026-02-25 05:42:50 -0800

    fix

--- ai4sar-webpage/lib/firebase-admin.ts
+++ ai4sar-webpage/lib/firebase-admin.ts
@@ -1,13 +1,13 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string,
-);
-
 if (!getApps().length) {
   initializeApp({
-    credential: cert(serviceAccount),
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
   });
 }
 
