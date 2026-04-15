37811b5db181502d2742599566ae071ceb7f33ac
Kevin Lu <kevinlu3234@gmail.com>
2026-04-14 19:26:29 -0700

    filler

---
+++ ai4sar-webpage/app/public/deep-learning/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function AboutPage() {
+  return (
+    <div className="page">
+      <main className="middle-layer">
+        <section className="hero-text" style={{ marginBottom: "10px" }}>
+          <h1 className="hero-title">Documents</h1>
+          <p className="hero-subtitle">Files</p>
+        </section>
+      </main>
+    </div>
+  );
+}
